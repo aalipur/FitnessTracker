04a61192fccbbb6ab893d52e11ee85f974aa79ae
aalipur <aalipur@icloud.com>
2022-04-05 09:40:22 +0300

    Initial Commit

---
+++ FitnessTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessTracker
+//
+//  Created by Анатолий Алипур on 05.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
